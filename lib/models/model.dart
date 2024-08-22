c4f09ed46613c4073031b29d6a164ec47853b96f
Sanzay-Chaudhary <kctsanzu@gmail.com>
2024-08-22 13:17:40 +0545

    created model

---
+++ lib/models/model.dart
@@ -0,0 +1,38 @@
+class Attachment {
+  final String url;
+  const Attachment({required this.url});
+}
+
+class Name {
+  final String first;
+  final String last;
+  String get fullName => '$first $last';
+
+  const Name({required this.first, required this.last});
+}
+
+class User {
+  final Name name;
+  final String avatarUrl;
+  final DateTime lastActive;
+
+  const User(
+      {required this.name, required this.avatarUrl, required this.lastActive});
+}
+
+class Email {
+  final User sender;
+  final List<User> recipients;
+  final String subject;
+  final String content;
+  final List<Attachment> attachment;
+  final double replies;
+
+  const Email(
+      {required this.sender,
+      required this.recipients,
+      required this.subject,
+      required this.content,
+      required this.attachment,
+      required this.replies});
+}
